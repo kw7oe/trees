45dfb7dce34231f481ed77eb0f581dfbd6aed8d1
kw7oe <choongkwern@hotmail.com>
2022-01-15 14:42:39 +0800

    Getting fcked by ownership and printing trees nicely

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 type Link = Option<Box<Node>>;
 
 struct Node {
@@ -8,24 +10,15 @@ struct Node {
 
 pub struct BSTree {
     root: Link,
-}
-
-impl std::fmt::Debug for BSTree {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut node = self.root.as_ref();
-
-        while let Some(n) = node {
-            write!(f, "{}", n.val)?;
-            node = n.right.as_ref();
-        }
-
-        write!(f, "END")
-    }
+    size: u32,
 }
 
 impl BSTree {
     pub fn new() -> BSTree {
-        BSTree { root: None }
+        BSTree {
+            root: None,
+            size: 0,
+        }
     }
 
     pub fn insert(&mut self, val: i32) -> i32 {
@@ -51,6 +44,7 @@ impl BSTree {
             if val > n.val {
                 if n.right.is_none() {
                     n.right = Some(Box::new(node));
+                    self.size += 1;
                     break;
                 } else {
                     temp = n.right.as_mut();
@@ -58,6 +52,7 @@ impl BSTree {
             } else {
                 if n.left.is_none() {
                     n.left = Some(Box::new(node));
+                    self.size += 1;
                     break;
                 } else {
                     temp = n.left.as_mut();
@@ -69,17 +64,79 @@ impl BSTree {
     }
 
     pub fn remove(&mut self, val: i32) -> bool {
+        if self.root.is_none() {
+            return false;
+        }
+
+        let mut node = self.root;
+        let mut prev = self.root.unwrap();
+
+        while let Some(n) = node {
+            // Found the node to remove.
+            //
+            // Actor invovled: Node Parents and Children
+            if n.val == val {
+                if let Some(left) = &prev.left {
+                    if left.val == val {
+                        prev.left = n.left.take()
+                    }
+                }
+
+                return true;
+            }
+
+            if n.val > val {
+                node = n.left;
+            } else {
+                node = n.right;
+            }
+
+            prev = n;
+        }
+
         false
     }
 
     pub fn get(&self, val: &i32) -> Option<&i32> {
-        if let Some(n) = self.root.as_ref() {
-            if n.val == *val {
+        let mut node = self.root.as_ref();
+
+        while let Some(n) = node {
+            if &n.val == val {
                 return Some(&n.val);
+            }
+
+            if &n.val > val {
+                node = n.left.as_ref();
+            } else {
+                node = n.right.as_ref();
             }
         }
 
         None
+    }
+
+    pub fn print(&self) {
+        if self.root.is_none() {
+            println!("None");
+        }
+
+        let mut queue: VecDeque<&Box<Node>> = VecDeque::new();
+        let node = self.root.as_ref().unwrap();
+        queue.push_back(node);
+
+        while !queue.is_empty() {
+            if let Some(n) = queue.pop_front() {
+                println!("{}", n.val);
+
+                if let Some(left) = &n.left {
+                    queue.push_back(left);
+                }
+
+                if let Some(right) = &n.right {
+                    queue.push_back(right);
+                }
+            }
+        }
     }
 }
 
@@ -97,8 +154,18 @@ mod test {
         assert_eq!(tree.insert(4), 4);
         assert_eq!(tree.insert(1), 1);
 
-        println!("{:?}", tree);
-        // assert_eq!(tree.get(&2), Some(&2));
-        // assert_eq!(tree.remove(2), true);
+        assert_eq!(tree.get(&2), Some(&2));
+        assert_eq!(tree.get(&3), Some(&3));
+        assert_eq!(tree.get(&6), None);
+
+        assert_eq!(tree.remove(2), true);
+        assert_eq!(tree.get(&2), None);
     }
+
+    // tree.insert(8);
+    // tree.insert(9);
+    // tree.insert(4);
+    // tree.insert(10);
+    // tree.insert(3);
+    // tree.insert(5);
 }
