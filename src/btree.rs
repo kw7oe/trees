1009f07206f273b3956849e8da25f859102cb8fd
kw7oe <choongkwern@hotmail.com>
2022-02-03 23:56:07 +0800

    Implement search and insert on/to child nodes

--- src/btree.rs
+++ src/btree.rs
@@ -4,14 +4,14 @@ pub struct BTree {
 
 #[derive(Debug)]
 struct Node {
-    numbers_of_keys: u32,      // 2t ^ h - 1.
+    numbers_of_keys: usize,    // 2t ^ h - 1.
     keys: Vec<u32>,            // At least t - 1 keys, at most 2t - 1 keys
     childrens: Vec<Box<Node>>, // At least t children, at most 2t children
     is_leaf: bool,
 }
 
-const MINIMUM_DEGREE: u32 = 2; // t
-const MAX_DEGREE: u32 = 2 * MINIMUM_DEGREE - 1;
+const MINIMUM_DEGREE: usize = 2; // t
+const MAX_DEGREE: usize = 2 * MINIMUM_DEGREE - 1;
 
 impl Node {
     pub fn new(is_leaf: bool) -> Self {
@@ -20,6 +20,41 @@ impl Node {
             keys: vec![],
             childrens: Vec::new(),
             is_leaf,
+        }
+    }
+
+    pub fn search(&self, key: &u32) -> Option<&u32> {
+        let mut index = 0;
+        let mut node_key = self.keys[index];
+
+        // println!(
+        //     "index: {}, key: {}, node_key: {}, nok: {}",
+        //     index, key, node_key, self.numbers_of_keys
+        // );
+
+        while *key > node_key {
+            index += 1;
+
+            // println!(
+            //     "index: {}, key: {}, node_key: {}, nok: {}",
+            //     index, key, node_key, self.numbers_of_keys
+            // );
+
+            if index >= self.numbers_of_keys {
+                break;
+            }
+
+            node_key = self.keys[index];
+        }
+
+        if index < self.numbers_of_keys && *key == node_key {
+            return self.keys.get(index);
+        } else if self.is_leaf {
+            return None;
+        } else {
+            let next_node = &self.childrens[index];
+            // println!("------\nGo to: {:?}", next_node);
+            return next_node.search(key);
         }
     }
 
@@ -38,22 +73,52 @@ impl Node {
                 }
             }
 
-            // Split childs to new node if not leaf node
+            // x.key(i) = y.key(t)
+            // where t = MINIMUM_DEGREE
+            if let Some(key) = child.keys.pop() {
+                self.keys.insert(index, key);
+                child.numbers_of_keys -= 1;
+            }
 
-            // Now assign both nodes to parents
+            // Split childs to new node if not leaf node
             self.childrens.push(Box::new(new_node));
+
+            // x.n = x.n + 1
+            self.numbers_of_keys += 1;
         };
     }
 
     pub fn insert_non_full(&mut self, key: u32) {
-        match self.keys.binary_search(&key) {
-            Ok(_) => (),
-            Err(pos) => {
-                self.keys.insert(pos, key);
+        if self.is_leaf {
+            match self.keys.binary_search(&key) {
+                Ok(_) => (),
+                Err(pos) => {
+                    self.keys.insert(pos, key);
+                }
+            }
+
+            self.numbers_of_keys += 1;
+        } else {
+            let mut index = self.numbers_of_keys - 1;
+
+            while index > 0 && key < self.keys[index] {
+                index -= 1;
+            }
+
+            if key > self.keys[index] {
+                index += 1;
+            }
+
+            if self.childrens[index].numbers_of_keys == MAX_DEGREE {
+                // Split again
+
+                if key > self.keys[index] {
+                    index += 1
+                }
+            } else {
+                self.childrens[index].insert_non_full(key)
             }
         }
-
-        self.numbers_of_keys += 1;
     }
 }
 
@@ -74,7 +139,7 @@ impl BTree {
                 node.insert_non_full(key);
             }
         } else {
-            let mut node = Node::new(false);
+            let mut node = Node::new(true);
             node.insert_non_full(key);
             self.root = Some(Box::new(node));
         }
@@ -86,9 +151,7 @@ impl BTree {
 
     pub fn get(&self, key: &u32) -> Option<&u32> {
         if let Some(node) = &self.root {
-            node.keys
-                .binary_search(key)
-                .map_or(None, |index| node.keys.get(index))
+            node.search(key)
         } else {
             None
         }
@@ -115,11 +178,11 @@ mod test {
         tree.insert(7);
         tree.insert(8);
         tree.insert(9);
-        // tree.insert(4);
+        tree.insert(4);
         // tree.insert(6);
         // tree.insert(1);
 
-        // tree.insert(5);
+        tree.insert(5);
 
         tree.print();
 
@@ -127,8 +190,8 @@ mod test {
         assert_eq!(tree.get(&7), Some(&7));
         assert_eq!(tree.get(&8), Some(&8));
         assert_eq!(tree.get(&9), Some(&9));
-        // assert_eq!(tree.get(&5), Some(&5));
-        // assert_eq!(tree.get(&10), None);
+        assert_eq!(tree.get(&5), Some(&5));
+        assert_eq!(tree.get(&10), None);
 
         // tree.remove(&7);
         // assert_eq!(tree.get(&7), None);
